0544dd0682e799e8fc2ec4ef449db8987fde94ed
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-08-07 23:44:16 -0400

    moved member back to Form and fixed clearing of form on submit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import Member from "./Member";
 
 function App() {
   const [team, setTeam] = useState([]);
-  const [member, setMember] = useState({
-    name: "Name",
-    email: "email",
-    role: "Role",
-})
+//   const [member, setMember] = useState({
+//     name: "Name",
+//     email: "email",
+//     role: "Role",
+// })
   console.log(team);
   return (
     <div>
-      <Form  member={member} setMember={setMember} setTeam={setTeam} team={team}/>
+      <Form  setTeam={setTeam} team={team}/>
       <Member team={team}/>
     </div>
   );
--- src/Form.js
+++ src/Form.js
@@ -2,18 +2,22 @@ import React, { useState } from "react";
 
 const Form = (props) => {
     console.log(props);
-    
+    const [member, setMember] = useState({
+        name: "",
+        email: "",
+        role: "",
+    })
     const Change = event => {
         const newMember = {
-            ...props.member,
+            ...member,
             [event.target.name]: event.target.value,
         }
-        props.setMember(newMember);
+        setMember(newMember);
     }
     const Submit = event => {
         event.preventDefault();
-        props.setTeam([...props.team, props.member]);
-        props.setMember({
+        props.setTeam([...props.team, member]);
+        setMember({
             name: "",
             email: "",
             role: ""
@@ -24,11 +28,11 @@ return (
         <h4>Add A Team Member</h4>
         <form onSubmit={Submit}> 
             <label htmlFor="name">Name: </label>
-            <input type="text" id="name" name="name" placeholder="Enter Name Here" onChange={Change}/>
+            <input type="text" id="name" name="name" placeholder="Enter Name Here" value={member.name} onChange={Change}/>
             <label htmlFor="email">Email: </label>
-            <input type="email" id="email" name="email" placeholder="Enter Email" onChange={Change}/>
+            <input type="email" id="email" name="email" placeholder="Enter Email" value={member.email} onChange={Change}/>
             <label htmlFor="role">Role: </label>
-            <input type="text" id="role" name="role" placeholder="Role" onChange={Change}/>
+            <input type="text" id="role" name="role" placeholder="Role" value={member.role} onChange={Change}/>
             <button type="submit">Submit</button>
 
         </form>
